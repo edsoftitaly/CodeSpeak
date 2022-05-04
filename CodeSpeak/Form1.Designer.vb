20305d114af0968b1374a14bf39941eadf43b668
rincewind1991 <epnmobile.en@gmail.com>
2022-05-04 13:21:52 +0200

    Add project files.

---
+++ CodeSpeak/Form1.Designer.vb
@@ -0,0 +1,228 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.English = New System.Windows.Forms.Button()
+        Me.ITALIAN = New System.Windows.Forms.Button()
+        Me.CustomCipherInput = New System.Windows.Forms.TextBox()
+        Me.CustomPlainInput = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.EncryptedOutput = New System.Windows.Forms.TextBox()
+        Me.NormalInput = New System.Windows.Forms.TextBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.English)
+        Me.GroupBox1.Controls.Add(Me.ITALIAN)
+        Me.GroupBox1.Controls.Add(Me.CustomCipherInput)
+        Me.GroupBox1.Controls.Add(Me.CustomPlainInput)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.EncryptedOutput)
+        Me.GroupBox1.Controls.Add(Me.NormalInput)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 16)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.GroupBox1.Size = New System.Drawing.Size(1015, 479)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Normal text to be encrypted"
+        '
+        'English
+        '
+        Me.English.Location = New System.Drawing.Point(808, 414)
+        Me.English.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.English.Name = "English"
+        Me.English.Size = New System.Drawing.Size(52, 57)
+        Me.English.TabIndex = 13
+        Me.English.Text = "EN"
+        Me.English.UseVisualStyleBackColor = True
+        '
+        'ITALIAN
+        '
+        Me.ITALIAN.Location = New System.Drawing.Point(736, 414)
+        Me.ITALIAN.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.ITALIAN.Name = "ITALIAN"
+        Me.ITALIAN.Size = New System.Drawing.Size(52, 57)
+        Me.ITALIAN.TabIndex = 12
+        Me.ITALIAN.Text = "IT"
+        Me.ITALIAN.UseVisualStyleBackColor = True
+        '
+        'CustomCipherInput
+        '
+        Me.CustomCipherInput.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower
+        Me.CustomCipherInput.Font = New System.Drawing.Font("Consolas", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.CustomCipherInput.Location = New System.Drawing.Point(9, 346)
+        Me.CustomCipherInput.Name = "CustomCipherInput"
+        Me.CustomCipherInput.PlaceholderText = "Custom key correspondance"
+        Me.CustomCipherInput.Size = New System.Drawing.Size(1000, 54)
+        Me.CustomCipherInput.TabIndex = 11
+        '
+        'CustomPlainInput
+        '
+        Me.CustomPlainInput.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower
+        Me.CustomPlainInput.Font = New System.Drawing.Font("Consolas", 24.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.CustomPlainInput.Location = New System.Drawing.Point(9, 286)
+        Me.CustomPlainInput.Name = "CustomPlainInput"
+        Me.CustomPlainInput.PlaceholderText = "Custom plain letters"
+        Me.CustomPlainInput.Size = New System.Drawing.Size(1000, 54)
+        Me.CustomPlainInput.TabIndex = 10
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(187, 414)
+        Me.Button4.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(243, 57)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "Convert with Custom Subsistution Cipher"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(7, 199)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(1002, 84)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = resources.GetString("Label1.Text")
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(438, 414)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(143, 57)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Convert to Farfallino"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(866, 414)
+        Me.Button5.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(143, 57)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Exit"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'EncryptedOutput
+        '
+        Me.EncryptedOutput.Location = New System.Drawing.Point(540, 32)
+        Me.EncryptedOutput.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.EncryptedOutput.Multiline = True
+        Me.EncryptedOutput.Name = "EncryptedOutput"
+        Me.EncryptedOutput.PlaceholderText = "Output"
+        Me.EncryptedOutput.ReadOnly = True
+        Me.EncryptedOutput.Size = New System.Drawing.Size(469, 163)
+        Me.EncryptedOutput.TabIndex = 3
+        '
+        'NormalInput
+        '
+        Me.NormalInput.Location = New System.Drawing.Point(7, 32)
+        Me.NormalInput.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.NormalInput.Multiline = True
+        Me.NormalInput.Name = "NormalInput"
+        Me.NormalInput.PlaceholderText = "Input"
+        Me.NormalInput.Size = New System.Drawing.Size(527, 163)
+        Me.NormalInput.TabIndex = 2
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(587, 414)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(143, 57)
+        Me.Button3.TabIndex = 1
+        Me.Button3.Text = "Clear"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(7, 414)
+        Me.Button1.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(174, 57)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Convert with Default Subsistution Cipher"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1041, 24)
+        Me.MenuStrip1.TabIndex = 3
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1041, 508)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CodeSpeak by EdSoft"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents EncryptedOutput As TextBox
+    Friend WithEvents NormalInput As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents CustomCipherInput As TextBox
+    Friend WithEvents CustomPlainInput As TextBox
+    Friend WithEvents English As Button
+    Friend WithEvents ITALIAN As Button
+    Friend WithEvents MenuStrip1 As MenuStrip
+End Class
---
+++ CodeSpeak/Form1.vb
@@ -0,0 +1,99 @@
+﻿Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private Shared Function Cipher(input As String, oldAlphabet As String, newAlphabet As String, ByRef output As String) As Boolean
+        output = String.Empty
+
+        If oldAlphabet.Length <> newAlphabet.Length Then
+            Return False
+        End If
+
+        For i As Integer = 0 To input.Length - 1
+            Dim oldCharIndex As Integer = oldAlphabet.IndexOf(Char.ToLower(input(i)))
+
+            If oldCharIndex >= 0 Then
+                output += If(Char.IsUpper(input(i)), Char.ToUpper(newAlphabet(oldCharIndex)), newAlphabet(oldCharIndex))
+            Else
+                output += input(i)
+            End If
+        Next
+
+        Return True
+    End Function
+
+    Public Shared Function Encipher(input As String, cipherAlphabet As String, ByRef output As String) As Boolean
+        Dim plainAlphabet As String = "aàbdeèégiìmnoòprs"
+        Return Cipher(input, plainAlphabet, cipherAlphabet, output)
+    End Function
+
+    Public Shared Function CustomEncipher(input As String, customPlain As String, cipherAlphabet As String, ByRef output As String) As Boolean
+
+        Return Cipher(input, customPlain, cipherAlphabet, output)
+    End Function
+
+
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        EncryptedOutput.Clear()
+        NormalInput.Clear()
+        CustomCipherInput.Clear()
+        CustomPlainInput.Clear()
+    End Sub
+
+
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        End
+    End Sub
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim text As String = NormalInput.Text 'gets the input from the text box aàbdeèégiìmnoòprs
+        Dim cipherAlphabet As String = "oòpgiìídeènmaàbsr" 'sets the cipherAlphabet according to the plain alphabet combinations
+        Dim cipherText As String = "" 'creates a variable that will contain the encrypted text
+        Encipher(text, cipherAlphabet, cipherText) 'encrypts the text
+        EncryptedOutput.Text = cipherText 'gives you the encrypted text
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        EncryptedOutput.Text = NormalInput.Text.Replace("a", "afa").Replace("e", "efe").Replace("i", "ifi").Replace("o", "ofo").Replace("u", "ufu").Replace("à", "afà").Replace("è", "efè").Replace("é", "efé").Replace("ì", "ifì").Replace("ò", "ofò").Replace("ù", "ufù").Replace("A", "AFA").Replace("E", "EFE").Replace("I", "IFI").Replace("O", "OFO").Replace("U", "UFU").Replace("À", "AFÀ").Replace("È", "EFÈ").Replace("É", "EFÉ").Replace("Ì", "IFÌ").Replace("Ò", "OFÒ").Replace("Ù", "UFÙ")
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim text As String = NormalInput.Text 'gets the input from the text box
+        Dim CustomPlainAlphabet As String = CustomPlainInput.Text
+        Dim cipherAlphabet As String = CustomCipherInput.Text 'sets the cipherAlphabet according to the plain alphabet combinations
+        Dim cipherText As String = "" 'creates a variable that will contain the encrypted text
+        Dim unused = CustomEncipher(text, CustomPlainAlphabet, cipherAlphabet, cipherText) 'encrypts the text
+        EncryptedOutput.Text = cipherText 'gives you the encrypted text
+    End Sub
+
+    Private Sub ITALIAN_Click(sender As Object, e As EventArgs) Handles ITALIAN.Click
+        Button1.Text = "Converti con cifrario standard"
+        Button4.Text = "Converti con cifrario personalizzato"
+        Button2.Text = "Converti in alfabeto farfallino"
+        Button3.Text = "Svuota"
+        Button5.Text = "Esci"
+        GroupBox1.Text = "Testo normale da crittografare"
+        Label1.Text = "Puoi scegliere se criptare/decriptare un messaggio usando il cifrario standard o creandone uno personalizzato. Nella prima casella di testo inserisci senza spazi le lettere o simboli che desideri siano sostituiti. Nella casella sottostante scrivi i corrispettivi simboli che cambieranno, senza spazi"
+        CustomPlainInput.PlaceholderText = "Alfabeto custom di partenza"
+        CustomCipherInput.PlaceholderText = "Conversione custom"
+    End Sub
+
+    Private Sub English_Click(sender As Object, e As EventArgs) Handles English.Click
+        Button1.Text = "Convert with Default Substitution Cipher"
+        Button4.Text = "Convert with Custom Substitution Cipher"
+        Button2.Text = "Convert to Farfallino"
+        Button3.Text = "Clear"
+        Button5.Text = "Exit"
+        GroupBox1.Text = "Normal text to be encrypted"
+        Label1.Text = "Choose between the standard cipher method or a custom one. In the first textbox you'll have to list the letters that will be replaced. In the textbox below, you'll write the corresponding letter or symbol you want to use to replace the single letter."
+        CustomPlainInput.PlaceholderText = "Custom plain letters"
+        CustomCipherInput.PlaceholderText = "Custom key correspondance"
+    End Sub
+End Class
